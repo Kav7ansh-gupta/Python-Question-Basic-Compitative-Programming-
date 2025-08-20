3c6ffbc8c06c3169bf8716dba1338c15ea7b9945
Kav7ansh-gupta <kavyanshgupta119@gmail.com>
2025-08-20 14:37:50 +0530

    patterns

---
+++ Week_4 Patterns with loop/GridPatterns.py
@@ -0,0 +1,7 @@
+row = int(input("Enter the numbr of rows : "))
+coloumns = int(input("Enter the numbr of coloumns : "))
+
+for i in range(row):
+    for j in range (coloumns):
+        print("*" , end=" ")
+    print()
