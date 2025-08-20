e7edd3fc970c8b4177eb3db8bab16c193c99e745
Kav7ansh-gupta <kavyanshgupta119@gmail.com>
2025-08-20 14:45:49 +0530

    Patterns

---
+++ Week_3 Loops/Kavyansh.py
@@ -0,0 +1,8 @@
+#To print Range into a list form        
+print(list(range(0,6))) 
+
+for i in range(1,10,2):
+    print(i)
+
+for i in range(1,10,2):
+    print(i,end="")
---
+++ Week_3 Loops/Power.py
@@ -0,0 +1,7 @@
+a = int(input("Enter a number : "))
+b = int(input("Enter a number : "))
+answer = 1
+
+for i in range(b):
+    answer = answer*a
+print(answer)
