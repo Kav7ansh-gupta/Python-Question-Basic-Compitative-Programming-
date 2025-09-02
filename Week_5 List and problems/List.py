f7970ecbca8e9b3b5d8e662bc2eaba000f9f4f9f
kavyansh gupta <kavyanshgupta119@gmail.com>
2025-09-02 10:45:56 +0530

    list

---
+++ Week_5 List and problems/List.py
@@ -0,0 +1,10 @@
+My_list = [33,79.9,"Kavyansh gupta",["bca",True]]
+# index ----- [0,1,2,3(0,1)]
+print(My_list)
+print("Name oF the student ", My_list[2])
+print("nickName oF the student ", My_list[2][0:5])
+print("RollNUmber oF the student ", My_list[0])
+print("Marks of the student ", My_list[1])
+
+My_list[1]=80
+print("Updated Marks of the student ", My_list[1])
---
+++ Week_5 List and problems/Sum_of_List.py
@@ -0,0 +1,5 @@
+my_list = [2,4,6,8,10,12]
+sum = 0
+for i in my_list:
+    sum+=i
+print(sum)
